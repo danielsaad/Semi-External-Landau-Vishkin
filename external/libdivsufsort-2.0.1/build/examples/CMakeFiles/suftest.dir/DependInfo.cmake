90a45790214fba919903e7d31e4a8eb7ef2e5d1b
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2015-08-14 16:33:51 -0300

    compiling fixes

---
+++ external/libdivsufsort-2.0.1/build/examples/CMakeFiles/suftest.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/daniel/Códigos/Semi-External-Landau-Vishkin/external/libdivsufsort-2.0.1/examples/suftest.c" "/home/daniel/Códigos/Semi-External-Landau-Vishkin/external/libdivsufsort-2.0.1/build/examples/CMakeFiles/suftest.dir/suftest.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "HAVE_CONFIG_H=1"
+  "_FILE_OFFSET_BITS=64"
+  "_LARGEFILE_SOURCE"
+  "_LARGE_FILES"
+  "__STDC_CONSTANT_MACROS"
+  "__STDC_FORMAT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/daniel/Códigos/Semi-External-Landau-Vishkin/external/libdivsufsort-2.0.1/build/lib/CMakeFiles/divsufsort.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../examples/../include"
+  "examples/../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
